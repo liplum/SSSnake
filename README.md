bb8eb69e17f7ad18fac5132b1c737439774199b7
Liplum <Li_plum@outlook.com>
2022-09-10 06:31:18 +0800

    init window size

---
+++ README.md
@@ -0,0 +1,2 @@
+# SSSnake
+A small swallowing-snake🐍 game powered by [LÖVE 💝](https://love2d.org).
--- SSSnake/main.lua
+++ SSSnake/main.lua
@@ -2,11 +2,19 @@ require("all")
 require("loader")
 
 love.window.setTitle("SSSnake")
+-- hardcode size
+TileSize = 32
+TileRow = 50
+TileColumn = 50
+WindowWidth = 800
+WindowHeight = 600
+love.window.setMode(WindowWidth, WindowHeight)
+PrintAPI(love, "love")
 
 function love.load()
     LoadContents(AllContents)
 end
 
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+    love.graphics.print("Hello World", WindowWidth / 2, WindowHeight / 2)
 end
